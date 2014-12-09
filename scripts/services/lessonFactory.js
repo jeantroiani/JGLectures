e18aceef3e9fa8c0115da621a60e8a80329b1434
unknown <jtroiani@LENOVOT440P-49.Justgiving.local>
2014-12-09 13:17:54 +0000

    adding factories

---
+++ scripts/services/lessonFactory.js
@@ -0,0 +1,11 @@
+(function () {
+	var lessonFactory = function () {
+		var lessons = [{name: "Jean", lesson: "angularJs", description: "Basics of Angular", date: "14:20 11-12-2014", where: "The Garage"}, {name: "Basem", lesson: "Responsive Design", description: "importance of Responsive design", date: "18:00 12-10-2014", where: "Big room"}, {name: "Neil", lesson: "Responsive images", description: "Fluid layout", date: "14:20 01-20-2015", where: "Secret room"}];
+		var factory = {};
+		factory.getLessons = function () {
+			return lessons;
+		};
+	};
+	
+	
+}());
